6e599deca0eacfd52fbd91abe94c4607f5771630
Rahul Rajput <rahulrajput21112@gmail.com>
2023-02-14 16:34:47 +0530

    Updated

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import style from '../styles/Home.module.css'
 import Navbar from '../Components/navbar'
 import { MdClose } from 'react-icons/md'
 import { useRouter } from 'next/router'
-import logout from '../Components/logout'
+import logOut from '../Components/logOut'
 
 
 
